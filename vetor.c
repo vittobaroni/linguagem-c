59bbbcc2be4af7d6778a5460651dd19f546b607e
Vitto Baroni <116370895+vittobaroni@users.noreply.github.com>
2023-10-08 22:24:02 -0300

    mostrar os numeros digitados

---
+++ vetor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    int i;
+    int n;
+    int q;
+    scanf("%d",&n);
+    int v[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&v[i]);
+    }
+    printf("Dados inseridos: ");
+    for(i=0;i<n;i++){
+        printf(" %d ",v[i]);
+    }
+    return 0;
+}
