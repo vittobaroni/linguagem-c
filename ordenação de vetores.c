6a03da1b08b551638c5bd674ea619880f2e8451d
Vitto Baroni <116370895+vittobaroni@users.noreply.github.com>
2023-11-30 10:23:37 -0300

    atualizado, com o passo a passo

--- ordenação de vetores.c
+++ ordenação de vetores.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
+
 int main(){
     
-    int i,auxiliar,x,n;
+    int i, n, aux, j;
     
-    printf("Tamanho do vetor: ");
+    // ler o tamanho do vetor:
+    printf("Digite o tamanho do vetor: ");
     scanf("%d",&n);
+    
+    // declarar o vetor, de acordo com o n lido
     int v[n];
-    for (i=0;i<n;i++){
-        printf("Posição %d: ",i+1);
-        scanf("%d",&v[i]);
-    }
+    
+    // colocar os valores do vetor
     for(i=0;i<n;i++){
-        for(x=i+1;x<n;x++){
-            
-            if(v[i] > v[x]){
-                auxiliar = v[i];
-                v[i] = v[x];
-                v[x] = auxiliar;
+        printf("Digite os valores dos vetores: ");
+        scanf("%d",&v[i]);
+        }
+    // colocar o método de ordenação
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            // agora vai mudar de lugar, usando um if
+            if(v[i] > v[j]){
+                //atribuição de valores
+                aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+                // eles vao se comparar. O que for maior, vao trocar de lugar
             }
         }
     }
-    
     printf("Ordem Crescente: \n");
+    // mais um for, pra repetir os valores
     for(i=0;i<n;i++){
         printf("%d\n",v[i]);
-    }
-    
+        }
     return 0;
 }
