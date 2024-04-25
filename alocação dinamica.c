235c4fe521d88e29b2bc5a7509da75f4069b6f48
Vitto Baroni <116370895+vittobaroni@users.noreply.github.com>
2024-04-25 02:18:05 -0300

    Create alocação dinamica.c

---
+++ alocação dinamica.c
@@ -0,0 +1,65 @@
+// Escreva uma função que recebe um inteiro N e retorna um vetor alocado 
+// dinamicamente contendo N valores inteiros dados por: {1, 2, 3, 4, 5, 6, … N}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// int* sequencia(int N){ // da erro se N for negativo ( nao conseguir puxar memoria)
+//   if (N<=0) {
+//     return NULL;
+//   }
+//   int* v = (int*)malloc(N*sizeof(int)); //aloca memoria
+  
+//   if ( v == NULL){
+//     return NULL;
+//   }
+//   for(int i = 0; i < N; i++){
+//     v[i] =i+1 ; // atribui o valor i + 1 ao i ésimo elemento do vetor
+//   }
+//   return v;
+// }
+// int main(){
+//  int* v = sequencia(100);
+//  for(int i = 0; i < 100; i++){
+//   printf("%d ", v[i]);
+//  }
+// free(v);
+// }
+
+int* sequencia(int N){
+  if(N<=0){
+    return NULL;
+  }
+  int* v = (int*)malloc(N*sizeof(int));
+  if(v == NULL){
+    return NULL;
+  }
+  for(int i = 0; i < N; i++){
+    v[i] = i + 1;
+  }
+  return v;
+}
+
+int* quadrados(int N,int v[N]){
+  if(N<=0){
+    return NULL;
+  }
+  int*u = (int*)malloc(N*sizeof(int));
+  if(u == NULL){
+    return NULL;
+  }
+  for(int i = 0; i < N; i++){
+    u[i] = v[i]*v[i];
+  }
+  return u;
+}
+
+
+int main(){
+ int* v = sequencia(100);
+ int* u = quadrados(100, v);
+ for(int i = 0; i < 100; i++)
+ printf("%d : %d\n", u[i], v[i]);
+ free(v);
+ free(u);
+}
